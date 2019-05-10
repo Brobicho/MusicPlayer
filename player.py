5933b43d865bfdfab82c8f230b009faafe9cac8f
brobicho <brobicho@student.le-101.fr>
2019-05-10 13:07:46 +0200

    1

--- player.py
+++ player.py
@@ -1,12 +1,40 @@
 from tkinter import *
+import os
+import tkinter.messagebox
+import tkinter.filedialog
 
-Window = Tk()
-Label1 = Label(Window, text = 'Bonjour tout le monde !', fg = 'red')
-Label1.pack()
+def Ouvrir():
+    file = tkinter.filedialog.askopenfilename(title="Ouvrir un fichier",filetypes=[('audio files','.mp3', '.mp4', '.wav', '.flac', '.wma'),('all files','.*')])
+    filename, file_extension = os.path.splitext(file)
+    print(file)
+    print(filename)
+    print(file_extension)
+    #window.title("SMP - " + str(photo.width()) + " x " + str(photo.height()))
 
-# Création d'un widget Button (bouton Quitter)
-btn = Button(Window, text = 'Quitter', command = Window.destroy)
-btn.pack()
+def Fermer():
+    window.title("SMP")
 
-# Lancement du gestionnaire d'événements
-Window.mainloop()
+def Apropos():
+    tkinter.messagebox.showinfo("A propos","Simple python music player made by brobicho")
+
+# Main window
+window = Tk()
+window.title("SMP")
+
+# Création d'un widget Menu
+menubar = Menu(window)
+
+menufichier = Menu(menubar,tearoff=0)
+menufichier.add_command(label="Ouvrir un fichier",command=Ouvrir)
+menufichier.add_command(label="Quitter", command=window.destroy)
+menubar.add_cascade(label="Fichier", menu=menufichier)
+
+menuaide = Menu(menubar,tearoff=0)
+menuaide.add_command(label="A propos",command=Apropos)
+menubar.add_cascade(label="Aide", menu=menuaide)
+
+# Affichage du menu
+window.config(menu=menubar)
+
+
+window.mainloop()
